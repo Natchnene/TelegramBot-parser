5d6c51fbd778775fa2912d279b35fe9cb435aa7d
Honor <kondratsiuk.marina@gmail.com>
2023-06-25 20:58:27 +0300

    add tgBot, parser

---
+++ urls_consts.py
@@ -0,0 +1,4 @@
+URL_LIGHT_VERSION = "http://m.flashscore.com.ua/"
+URL_LIGHT_VERSION_OVER = "http://m.flashscore.com.ua/?d=0&s=3"
+URL_LIGHT_VERSION_LIVE = "http://m.flashscore.com.ua/?s=2"
+URL_NEWS = "https://www.sports.ru/football/"
